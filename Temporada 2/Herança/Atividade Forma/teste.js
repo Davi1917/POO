37b0a65bb65f7f39f1ee98e8aea20aea7a767c5c
Davi1917 <130773377+Davi1917@users.noreply.github.com>
2023-10-09 10:47:46 -0300

    Update teste.js

--- Temporada 2/Herança/Atividade Forma/teste.js
+++ Temporada 2/Herança/Atividade Forma/teste.js
@@ -1,8 +1,29 @@
 let forma1 = new Quadrado('Roxo', 4)
 let forma2 = new Circulo('Vermelho', 5)
+let forma3
 
 forma1.descrever()
 forma1.calcularArea()
 
 forma2.descrever()
 forma2.calcularArea
+
+
+
+if (forma1 instanceof Forma) {
+    console.log('É uma forma geométrica válida')
+} else {
+    console.error('NÃO! é uma forma geométrica válida')
+}
+
+if (forma2 instanceof Forma) {
+    console.log('É uma forma geométrica válida')
+} else {
+    console.error('NÃO É UMA FORMA GEOMÉTRICA VÁLIDA!')
+}
+
+if (forma3 instanceof Forma) {
+    console.log('É uma forma geométrica válida')
+} else {
+    console.error('NÃO É UMA FORMA GEOMÉTRICA VÁLIDA!')
+}
