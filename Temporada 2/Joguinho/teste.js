cb693291d69cc376920ef12feea7300c896c67df
Davi1917 <130773377+Davi1917@users.noreply.github.com>
2023-10-17 10:46:07 -0300

    Create teste.js

---
+++ Temporada 2/Joguinho/teste.js
@@ -0,0 +1,10 @@
+let personagem1 = new Personagem('Estados Unidos', 1945, 100)
+let personagem2 = new Personagem('Japão', 1945, 100)
+
+console.log(personagem1)
+console.log(personagem2)
+
+personagem1.atacar(personagem2)
+
+
+
