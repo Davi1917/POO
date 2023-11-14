8466211bb6369c9f426fc02e0e9c4ac20831cf81
Davi1917 <130773377+Davi1917@users.noreply.github.com>
2023-11-14 10:50:51 -0300

    Create objetos.js

---
+++ Temporada 2/Projeto Final/Tela de lista/objetos.js
@@ -0,0 +1,43 @@
+let remedio1 = new Remedio(
+    "1", 
+    "Ibuprofeno",
+    "2,80", 
+    "123/1",
+    "ibuprofeno.png",
+    "2"
+);
+let remedio2 = new Remedio(
+    "2", 
+    "Buscofem", 
+    "2,80", 
+    "123/1", 
+    "buscofem.jpg", 
+    "2"
+);
+let remedio3 = new Remedio(
+    "3", 
+    "Ivermactina", 
+    "2,80", 
+    "123/1", 
+    "ivermectina.png", 
+    "2"
+);
+let remedio4 = new Remedio(
+    "4", 
+    "Buscopan", 
+    "2,80", 
+    "123/1", 
+    "buscopan.jpg", 
+    "2"
+);
+let remedio5 = new Remedio(
+    "5", 
+    "Rivotril", 
+    "2,80", 
+    "123/1", 
+    "rivotril.png", 
+    "2"
+);
+let listaDeRemedios = [];
+listaDeRemedios.push(remedio1, remedio2, remedio3, remedio4, remedio5)
+
